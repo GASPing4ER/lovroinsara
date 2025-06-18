5be591e28a59fbaf0bfdfea6738bf17a152de449
GASPing4ER <simecgasper@gmail.com>
2025-06-18 21:20:54 +0200

    časovnica za poroko

--- components/info/info.js
+++ components/info/info.js
@@ -1,28 +1,17 @@
-import Image from "next/image";
 import styles from "./info.module.css";
-import program from "@/public/program.svg";
 
 const Info = () => {
   return (
-    <>
-      <div className={styles.coming_soon}>VEČ INFORMACIJ KMALU!</div>
-      {/* <div className={styles.info}>
-        <Image src={program} alt="časovnica" />
-      </div>
-      <div className={styles["info_mobile"]}>
-        <h1>14:30 Zbor svatov na parkirišču</h1>
-        <h1>15:00 Cerkvena poroka</h1>
-        <h1>16:00 Pogostitev po obredu</h1>
-        <h1>18:00 Zbor svatov pred hotelom Dolenj’c</h1>
-        <h1>18:15 Prihod ženina in neveste, čestitke</h1>
-        <h1>19:30 Slavnostni prihod v dvorano</h1>
-        <h1>19:45 Juha in topla predjed</h1>
-        <h1>20:30 Prvi ples in program</h1>
-        <h1>21:30 Glavna jed</h1>
-        <h1>22:00 Program</h1>
-        <h1>23:30 Torta</h1>
-      </div> */}
-    </>
+    <div className={styles["info_mobile"]}>
+      <p>14:30 Molitev rožnega venca</p>
+      <p>15:00 Poročna maša</p>
+      <p>18:00 Odhod proti ranču</p>
+      <p>18:30 Aperitiv</p>
+      <p>19:45 Postrežba juhe</p>
+      <p>00:00 Torta</p>
+      <p>00:30 Presenečenje</p>
+      <p>Žur do jutranjih ur (02:00 burgerji)</p>
+    </div>
   );
 };
 
